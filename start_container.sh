28b676ef38c3d92a0be421dbe9b5f503c481728b
yogesh <117835365+yogeshpenumur@users.noreply.github.com>
2025-05-21 16:12:40 +0530

    start update

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull yogeshpenumur/my-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 /yogeshpenumur/my-app
+
